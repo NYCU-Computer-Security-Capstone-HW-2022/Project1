299ad1a3d84e671df684721cb9cf83e729791c0a
qwe854896 <qwe854896@gmail.com>
2022-02-27 19:29:14 +0800

    Remove for-loop and use string's constructor

--- dns_attack.cpp
+++ dns_attack.cpp
@@ -12,13 +12,13 @@ int main(int argc, char* argv[]) {
 		return 0;
 	}
 
-	std::string Victim_IP = "", Server_IP = "";
+	std::string Victim_IP(argv[1]), Server_IP(argv[3]);
 	int Source_Port = atoi(argv[2]);
 
-	for (int i = 0; i < strlen(argv[1]); ++i)
-		Victim_IP.push_back(argv[1][i]);
-	for (int i = 0; i < strlen(argv[3]); ++i)
-		Server_IP.push_back(argv[3][i]);
+	// for (int i = 0; i < strlen(argv[1]); ++i)
+	//	Victim_IP.push_back(argv[1][i]);
+	// for (int i = 0; i < strlen(argv[3]); ++i)
+	//	Server_IP.push_back(argv[3][i]);
 
 	dns_attack attack(Victim_IP, Source_Port, Server_IP);
 	attack.attack();
