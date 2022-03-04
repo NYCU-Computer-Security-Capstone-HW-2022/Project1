a22517edcb8820c0719472501005f16f351aa1c0
qwe854896 <qwe854896@gmail.com>
2022-03-04 16:13:26 +0800

    Remove comment

--- dns_attack.cpp
+++ dns_attack.cpp
@@ -15,11 +15,6 @@ int main(int argc, char* argv[]) {
 	std::string Victim_IP(argv[1]), Server_IP(argv[3]);
 	int Source_Port = atoi(argv[2]);
 
-	// for (int i = 0; i < strlen(argv[1]); ++i)
-	//	Victim_IP.push_back(argv[1][i]);
-	// for (int i = 0; i < strlen(argv[3]); ++i)
-	//	Server_IP.push_back(argv[3][i]);
-
 	dns_attack attack(Victim_IP, Source_Port, Server_IP);
 	attack.attack();
 
